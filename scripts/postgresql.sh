46b1d5c7efcad1e2b650a89cd9a069a0fd7596fa
Thomas Heslin <tjheslin1@gmail.com>
2017-05-06 13:31:47 +0100

    Adding start command to postgresql scripts

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -1,5 +1,6 @@
 # https://www.codementor.io/devops/tutorial/getting-started-postgresql-server-mac-osx
 #
+# postgres -D /usr/local/var/postgres
 # psql go_scheduler_db -U go_scheduler;
 #
 # \connect super_awesome_application
